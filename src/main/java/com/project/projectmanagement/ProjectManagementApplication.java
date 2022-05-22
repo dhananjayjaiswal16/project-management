35cc16908e17909cd9bef2b136344f8f8ed711d3
Dhananjay <dhananjayman11@gmail.com>
2022-05-22 11:08:40 +0530

    getting started

---
+++ src/main/java/com/project/projectmanagement/ProjectManagementApplication.java
@@ -0,0 +1,14 @@
+package com.project.projectmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementApplication.class, args);
+    System.out.println("Up and RUnning...");
+	}
+
+}
